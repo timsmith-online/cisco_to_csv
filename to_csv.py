9611e51d1f2ccb86357ff0d447269aa8916e6787
Tim Smith <135654150+timsmith-online@users.noreply.github.com>
2024-11-27 09:58:24 -0600

    Update to_csv.py

--- to_csv.py
+++ to_csv.py
@@ -1,48 +1,71 @@
 import pandas as pd
-import numpy as np
-import sys
+import os
+import xlsxwriter
 
-# args from zsh script
-script_name = sys.argv[0]
-if len(sys.argv) > 1:
-	first_arg = sys.argv[1]
-	second_arg = sys.argv[2]
-else:
-	print("No argument provided.")
+cur_dir = "/Users/timothy.s/port_map"
 
 
-data = pd.read_csv('./output.csv')
+data = pd.read_csv('dhcp_snooping.csv')
+data1 = pd.read_csv('outputs.csv')
+data2 = pd.read_csv('brief.csv')
+data3 = pd.read_csv('status.csv')
+
+list_csv = [data, data1, data2, data3]
+name_csv = ['dh.csv', 'out.csv', 'bri.csv', 'stat.csv']
+
+for csv_file in range(len(list_csv)):
+	cur_df = list_csv[csv_file]
+	if 'interface' in cur_df.columns:
+		cur_df["interface"] = cur_df["interface"].replace({
+			'FastEthernet': 'Fa',
+		  'TenGigabitEthernet': 'Te',
+		  'TwentyFiveGigabitEthernet': 'Twe',
+		  'FortyGigabitEthernet': 'Fo',
+		  'HundredGigabitEthernet': 'Hu',
+		  'TwoHundredGigabitEthernet': 'Two',
+		  'FourHundredGigabitEthernet': 'Fo',
+		  'GigabitEthernet': 'Gi',
+		  'TwentyFiveGigE': 'Twe',
+		  'HundredGigE': 'Hu',
+		  'Ethernet': 'Et',
+		  'Serial': 'Se',
+		  'ATM': 'At',
+		  'POS': 'Po',
+		  'Loopback': 'Lo',
+		  'Vlan': 'Vl',
+		  'Port-channel': 'Po',
+		  'Tunnel': 'Tu',
+		  'Apphosting': 'Ap',
+		  'Interface_ign': 'In',
+		  'Management': 'Mg',
+		  'Router': 'Ro',
+		  'Switch': 'Sw'}, regex=True)
+		cur_df.to_csv(name_csv[csv_file])
+	else:
+		cur_df.to_csv(name_csv[csv_file])
+
+data = pd.read_csv('dh.csv')
+data1 = pd.read_csv('out.csv')
+data2 = pd.read_csv('bri.csv')
+data3 = pd.read_csv('stat.csv')
+
 df = pd.DataFrame(data)
+df1 = pd.DataFrame(data1)
+df2 = pd.DataFrame(data2)
+df3 = pd.DataFrame(data3)
 
-
-df['data_vlan'] = df[['switchport access']]
-
-# Merge access and voice vlans -> to int -> to str
-df['vla'] = df[['switchport access', 'switchport voice']].apply(lambda x: ','.join(x.dropna().astype(int).astype(str)), axis=1)
-
-# Merge trunk ports -> to str
-df['vlans'] = df[['switchport trunk', 'vla']].apply(lambda x: ','.join(x.dropna().astype(str)), axis=1)
-# object to str
-df['vlans'] = df['vlans'].astype('string')
-
-# test code
-# df['vlans'] = df[['switchport trunk', 'vla']].apply(lambda x: ','.join(x.dropna().astype(int).astype(str)), axis=1)
-# df['vl'] = df['vlans'].apply(lambda x: x.rstrip(','))
-#non_numeric = df[df['vlans'].apply(lambda x: not str(x).isdigit())]
-#df['yach'] = df[df['vlans'].apply(lambda x: not str(x).isdigit())]
-
-# If 2 vlan tagged, not, untagged
-df['vlan_type'] = df['vlans'].apply(lambda x: 'tagged' if ',' in x else 'untagged')
-
-# No tag if interface = vlan
-df.loc[df['interface'].str.contains('Vlan', na=False), 'vlan_type'] = np.nan
-
-# Columns
-df = df[['interface', 'description', 'vlans', 'data_vlan', 'vlan_type', 'switchport mode']]
-# df = df[['interface', 'description', 'vlans', 'data_vlan', 'vlan_type', 'switchport mode', 'vl']]
-print(df.dtypes)
-
-# get from site name from DB or /etc/hosts
-first_arg = first_arg + "_" + second_arg + ".csv"
-
-df.to_csv(first_arg, index=False)
+df = df[['interface', 'ip_address', 'mac_address']]
+y = df.dropna().empty
+if y == True:		
+	combined_df = pd.merge(df1, df2, on="interface", how="right")
+	combined_df = pd.merge(df3, combined_df, on="interface", how="right")
+	combined_df = combined_df[['interface', 'description', 'vlans', 'data_vlan', 'vlan_type', 'switchport mode', 'status_y', 'duplex', 'speed', 'type']]
+	with pd.ExcelWriter('final.xlsx', engine='xlsxwriter') as writer: 
+		combined_df.to_excel(writer, sheet_name='sheet1', index=False)
+else:
+	combined_df = pd.merge(df, df1, on="interface", how="right")
+	combined_df = pd.merge(df2, combined_df, on="interface", how="right")
+	combined_df = pd.merge(df3, combined_df, on="interface", how="right")
+	combined_df = combined_df[['interface', 'status_y', 'ip_address', 'mac_address', 'description', 'vlans', 'data_vlan', 'vlan_type', 'switchport mode', 'duplex', 'speed', 'type']]
+	with pd.ExcelWriter('final.xlsx', engine='xlsxwriter') as writer: 
+		combined_df.to_excel(writer, sheet_name='sheet1', index=False)
